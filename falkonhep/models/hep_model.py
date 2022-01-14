dc56cc72af28939e7c28039ed60eb80984cdb16a
Akatsuki96 <marco.rando0396@gmail.com>
2022-01-14 10:49:25 +0000

    Modified generate_dataset function and minor changes

--- falkonhep/models/hep_model.py
+++ falkonhep/models/hep_model.py
@@ -29,26 +29,26 @@ class HEPModel:
         self.options = options
         os.makedirs(output_path, exist_ok=True)
 
-    def __generate_nosignal(self, R, B, S, features, cut_mll, normalize, ref_state):
+    def __generate_nosignal(self, R, B, S, features, cut, normalize, ref_state):
         bkg_size = ref_state.poisson(lam=B)
-        bkg, c_vect_bkg = read_data(R + bkg_size, features, self.reference_path, ref_state, cut_mll)
+        bkg, c_vect_bkg = read_data(R + bkg_size, features, self.reference_path, ref_state, cut)
         reference = bkg[:R,:]
         bkg = bkg[R:, :]
         if normalize:
             reference, bkg = normalize_features(reference, bkg)
-        if cut_mll is not None:
+        if cut is not None:
             c_vect_ref, c_vect_bkg = c_vect_bkg[ : R], c_vect_bkg[R : ]
             return reference[c_vect_ref, :], bkg[c_vect_bkg, :], len(bkg[c_vect_bkg, :]), None
         return reference, bkg, bkg_size, None
 
-    def __generate_resonant(self, R, B, S, features, cut_mll, normalize, ref_state, sig_state):
+    def __generate_resonant(self, R, B, S, features, cut, normalize, ref_state, sig_state):
         bkg_size = ref_state.poisson(lam=B)
         sig_size = sig_state.poisson(lam=S)
-        bkg, c_vect_bkg = read_data(R + bkg_size, features, self.reference_path, ref_state, cut_mll)
+        bkg, c_vect_bkg = read_data(R + bkg_size, features, self.reference_path, ref_state, cut)
         reference = bkg[:R, :]
         bkg = bkg[R:, :]
-        sig, c_vect_sig = read_data(sig_size, features, self.data_path, sig_state, cut_mll)
-        if cut_mll is not None:
+        sig, c_vect_sig = read_data(sig_size, features, self.data_path, sig_state, cut)
+        if cut is not None:
             c_vect_ref, c_vect_bkg = c_vect_bkg[:R], c_vect_bkg[R:]
             reference = reference[c_vect_ref, :]
             data = np.vstack((bkg[c_vect_bkg, :], sig[c_vect_sig, :]))
@@ -60,17 +60,17 @@ class HEPModel:
             reference, data = normalize_features(reference, data)
         return reference, data, bkg_size, sig_size
 
-    def __generate_nonresonant(self, R, B, S, features, cut_mll, normalize, ref_state, sig_state):
-        reference, c_vect_ref = read_data(R, features, reference_path, ref_state, cut_mll)
+    def __generate_nonresonant(self, R, B, S, features, cut, normalize, ref_state, sig_state):
+        reference, c_vect_ref = read_data(R, features, reference_path, ref_state, cut)
         bkg_size = sig_state.poisson(lam=B + S)
-        bkg, c_vect_bkg = read_data(bkg_size, features, self.data_path, sig_state, cut_mll)
+        bkg, c_vect_bkg = read_data(bkg_size, features, self.data_path, sig_state, cut)
         if normalize:
             reference, bkg = normalize_features(reference, bkg)
-        if cut_mll is not None:
+        if cut is not None:
             return reference[c_vect_ref, :], bkg[c_vect_bkg, :], len(bkg[c_vect_bkg, :]), None
         return reference, bkg, bkg_size, None
 
-    def generate_dataset(self, R, B, S, features, cut_mll, normalize, sig_type, ref_state, sig_state):
+    def generate_dataset(self, R, B, S, features, cut, normalize, sig_type, ref_seed, sig_seed):
         """Generate dataset
 
         Args:
@@ -78,7 +78,7 @@ class HEPModel:
             B (int): Expected background size
             S (int): Expected signal size
             features (list): List of features
-            cut_mll (float): Cut applied to mll
+            cut (tuple[str, float]): Cut applied to mll
             normalize (bool): If true normalization is applied
             sig_type (int): Type of signal applied (admitted 0: no-signal, 1: resonant, 2: non-resonant)
             ref_state (np.random.RandomSeed): pseudorandom generator used for background size
@@ -90,12 +90,13 @@ class HEPModel:
         Returns:
             (np.ndarray, np.ndarray, int, int (or None)): reference data, data-sample, background size, signal size (if sig_type=1)
         """        
+        ref_state, sig_state = np.random.RandomState(ref_seed), np.random.RandomState(sig_seed)
         if sig_type == 0:
-            return self.__generate_nosignal(R, B, S, features, cut_mll, normalize, ref_state)
+            return self.__generate_nosignal(R, B, S, features, cut, normalize, ref_state)
         elif sig_type == 1:
-            return self.__generate_resonant(R, B, S, features, cut_mll, normalize, ref_state, sig_state)
+            return self.__generate_resonant(R, B, S, features, cut, normalize, ref_state, sig_state)
         elif sig_type == 2:
-            return self.__generate_nonresonant(R, B, S, features, cut_mll, normalize, ref_state, sig_state)
+            return self.__generate_nonresonant(R, B, S, features, cut, normalize, ref_state, sig_state)
         raise Exception("Unknown signal type")
 
     def create_labels(self, ref_size, data_size):
@@ -123,7 +124,7 @@ class HEPModel:
         raise NotImplementedError("This function is not implemented in general class HEPModel")
 
     def learn_t(self, R:int, B:int, S:int, features:list, model_parameters:dict, sig_type:int, 
-                cut:tuple = None, normalize:bool = False, seeds:tuple = None, nratio_option = None):       
+                cut:tuple = None, normalize:bool = False, seeds:tuple = None):       
         """Method used to compute the t values 
 
         Args:
@@ -133,18 +134,19 @@ class HEPModel:
             features (list[str]): List containing the name of the features used
             model_parameters (dict): Dictionary containing the parameters for the model used
             sig_type (int): Type of signal (0: no-signal, 1: resonant, 2: non-resonant).
-            cut_mll (int, optional): Cut MLL. Defaults to None.
+            cut (int, optional): Cut MLL. Defaults to None.
             normalize (bool, optional): If True data will be normalized before fitting the model. Defaults to False.
             seeds (tuple[int, int], optional): A tuple (reference_seed, data_seed) used to generate reference and data sample, if None two random seeds are generated. Defaults to None.
-            nratio_option (list[str], optional): List of features to perform predictions. Defaults to None.
+            pred_features (list[str], optional): List of features to perform predictions. Defaults to None.
 
         """        
         ref_seed, data_seed = seeds if seeds is not None else generate_seeds(np.random.randint(100))
-        ref_state, data_state = np.random.RandomState(ref_seed), np.random.RandomState(data_seed)
+        #ref_state, data_state = np.random.RandomState(ref_seed), np.random.RandomState(data_seed)
 
-        preds = None # used if nratio_option is not None
+        preds = None # used if pred_features is not None
         
-        reference, data_sample, bck_size, sig_size = self.generate_dataset(R, B, S, features, cut, normalize, sig_type, ref_state, data_state)
+        reference, data_sample, bck_size, sig_size = self.generate_dataset(R, B, S, features, cut, 
+            normalize, sig_type, ref_seed, data_seed)
         
         data = np.vstack((reference, data_sample))
         data_size = bck_size + sig_size if sig_size is not None else bck_size
@@ -173,15 +175,7 @@ class HEPModel:
         t = 2 * (diff + torch.sum(data_pred).item())
 
         del data_pred, reference, data_sample, Xtorch, Ytorch
-        
-        if nratio_option is not None:
-            # predictions with cut?? (change None with cut)
-            # if nratio_option map => features = nratio_option['features']
-            pr_data, _ = read_data(R, nratio_option, self.reference_path, np.random.RandomState(ref_seed), None)
-            preds = self.predict(model, pr_data)
-            
-        
-        return t, Nw, train_time, ref_seed, data_seed, preds#ref_pred
+        return t, Nw, train_time, ref_seed, data_seed, ref_pred.numpy().reshape(-1)#ref_pred
 
     #def estimate_sigma(self, sample_size, features, rnd_state: np.random.RandomState = np.random.RandomState(42), verbose:bool=False):
     #    """Compute the pairwise (euclidean) distance in a sample of reference data and 
@@ -201,6 +195,22 @@ class HEPModel:
     #    dist = [for x in np.tril(pairwise_distances(ref_sample)) if x != 0]
     #    perc = np.percentile(dist, [90])
     #    return perc
+
+  #  def get_toy(self, R, B, S, ref_seed, sig_seed, features, cut, sig_type, reference_pred):
+  #      """Compute n ratio for a given toy and prediction
+#
+  #      Args:
+  #          R (int): [description]
+  #          B (int): [description]
+  #          S (int): [description]
+  #          feature (str): feature to extract
+  #          reference_pred (list[float]): Prediction made on reference sample (obtainable with learn_t)
+  #      """        
+  #      return self.generate_dataset(R, B, S, features, cut, False, sig_type,
+  #          np.random.RandomState(ref_seed), np.random.RandomState(sig_seed))
+  #      #return ref, data, bck
+  #      #pass
+
 
     def save_result(self, fname, i, t, Nw, train_time, ref_seed, sig_seed):
         """Function which save the result of learn_t in a file
