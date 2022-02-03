a2ccffa26e275846c4794845cafce2035d576ea8
Akatsuki96 <marco.rando0396@gmail.com>
2022-02-03 11:11:56 +0000

    Added LogFalkon Susy experiment (only low level features)

---
+++ examples/logflk_susy_lowlevel.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+import argparse as ap
+
+from falkonhep import LogFalkonHEPModel
+from falkonhep.utils import plot_reference, plot_sig
+
+R = 500000
+B = 100000
+
+features = ['lep1_eta', 'lep1_pT', 'lep1_phi', 'lep2_eta', 'lep2_pT', 'lep2_phi', 'missen_mag', 'missen_phi']
+
+
+def execute_experiment(reference_path, data_path, output_path, sigtype, S, ntoys_ref, ntoys_data):
+
+    rnd_state = np.random.RandomState(12)
+    print("[--] S = {}".format(S))
+    model_parameters = {
+        'sigma' : 4.5,
+        'penalty_list' : [1e-6],
+        'iter_list' : [1000000],
+        'M' : 10000,
+        'keops_active': "no",
+        'seed' : 12,
+        'cg_tol' : np.sqrt(1e-7)
+    }
+    model = LogFalkonHEPModel(reference_path, data_path, output_path, None)
+
+    for i in range(ntoys_ref):
+        model_parameters['seed'] = rnd_state.randint(low = 0, high = 2**32)
+        t, Nw, train_time, ref_seed, sig_seed = model.learn_t(R, B, 0, features, model_parameters, sig_type=0, normalize = False)
+        print("[REF] i: {}/{}\tt: {}\tNw: {}\t training time: {}".format(i+1, ntoys_ref, t, Nw, train_time))
+        model.save_result("reference.log", i, t, Nw, train_time, ref_seed, sig_seed)
+    for i in range(ntoys_data):
+        model_parameters['seed'] = rnd_state.randint(low = 0, high = 2**32)
+        t, Nw, train_time, ref_seed, sig_seed = model.learn_t(R, B, S, features, model_parameters, sig_type=sigtype, normalize = False)
+        print("[SIG] i: {}/{}\tt: {}\tNw: {}\t training time: {}".format(i+1, ntoys_data, t, Nw, train_time))
+        model.save_result("signal.log", i, t, Nw, train_time, ref_seed, sig_seed)
+    
+    plot_reference(model.output_path + "/reference.log", "SUSY (low level) reference", model.output_path + "/reference", bins=6, verbose = False)
+    plot_sig(model.output_path + "/reference.log", model.output_path + "/signal.log", "SUSY (low level)", model.output_path + "/signal", bins=6, verbose = True)
+
+if __name__ == "__main__":
+
+    parser = ap.ArgumentParser()
+    parser.add_argument('reference_path', type=str, help="Path for reference")
+    parser.add_argument('data_path', type=str, help="Path for data")
+    parser.add_argument('output_path', type=str, help="Path for output")
+
+    parser.add_argument('sigtype', type=int, choices=[0, 1, 2], help="Signal type:\n0: no signal\n1: resonant\n2: non-resonant")
+    parser.add_argument('NS', type=int, help="Expected signal size")
+
+    parser.add_argument('--ntoys-ref', type=int, default=1, help="Number of toys for reference")
+    parser.add_argument('--ntoys-data', type=int, default=1, help="Number of toys for data")
+
+    args = parser.parse_args()
+
+    reference_path = args.reference_path 
+    data_path = args.data_path 
+    output_path = args.output_path 
+    sigtype = args.sigtype
+    NS = args.NS
+    ntoys_ref, ntoys_data = args.ntoys_ref, args.ntoys_data
+
+    execute_experiment(reference_path, data_path, output_path, sigtype, NS, ntoys_ref, ntoys_data)
